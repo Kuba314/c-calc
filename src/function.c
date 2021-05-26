5a5e0747658e2587069ca7e9985a57025bf63f13
Kuba314 <tekno001@seznam.cz>
2021-05-26 21:43:28 +0200

    add ! and %, but with compromises

--- src/function.c
+++ src/function.c
@@ -18,20 +18,24 @@ static complex_t crand() {
     register long double denom = 1.F / sqrtl(real*real + imag*imag);
     return real * denom + imag * denom * I;
 }
+static complex_t gamma_complex(complex_t a) {
+    return tgammal(cabsl(a));
+}
 
 static struct func_def_t FUNCTIONS[] = {
-    {.name="abs"  , .nargmap=NARG_1, .c1=_cabsl},
-    {.name="sqrt" , .nargmap=NARG_1, .c1=csqrtl},
-    {.name="root" , .nargmap=NARG_2, .c2=crootl},
-    {.name="power", .nargmap=NARG_2, .c2=cpowl },
-    {.name="sin"  , .nargmap=NARG_1, .c1=csinl },
-    {.name="cos"  , .nargmap=NARG_1, .c1=ccosl },
-    {.name="tan"  , .nargmap=NARG_1, .c1=ctanl },
-    {.name="asin" , .nargmap=NARG_1, .c1=casinl},
-    {.name="acos" , .nargmap=NARG_1, .c1=cacosl},
-    {.name="atan" , .nargmap=NARG_1, .c1=catanl},
-    {.name="rand" , .nargmap=NARG_0, .c0=frand },
-    {.name="crand", .nargmap=NARG_0, .c0=crand },
+    {.name="abs"  , .nargmap=NARG_1, .c1=_cabsl       },
+    {.name="sqrt" , .nargmap=NARG_1, .c1=csqrtl       },
+    {.name="root" , .nargmap=NARG_2, .c2=crootl       },
+    {.name="power", .nargmap=NARG_2, .c2=cpowl        },
+    {.name="sin"  , .nargmap=NARG_1, .c1=csinl        },
+    {.name="cos"  , .nargmap=NARG_1, .c1=ccosl        },
+    {.name="tan"  , .nargmap=NARG_1, .c1=ctanl        },
+    {.name="asin" , .nargmap=NARG_1, .c1=casinl       },
+    {.name="acos" , .nargmap=NARG_1, .c1=cacosl       },
+    {.name="atan" , .nargmap=NARG_1, .c1=catanl       },
+    {.name="rand" , .nargmap=NARG_0, .c0=frand        },
+    {.name="crand", .nargmap=NARG_0, .c0=crand        },
+    {.name="gamma", .nargmap=NARG_1, .c1=gamma_complex},
 };
 
 const struct func_def_t *get_function(const char *fname, uint8_t nargs) {
--- src/operator.c
+++ src/operator.c
@@ -13,14 +13,23 @@ complex_t nop_complex(complex_t a) { return a; }
 complex_t neg_complex(complex_t a) { return -a; }
 complex_t root_complex(complex_t a, complex_t b) { return cpowl(b, 1 / a); }
 
+complex_t mod_complex(complex_t a, complex_t b) {
+    complex_t x = a / b;
+    x = floorl(creall(x)) + floorl(cimagl(x)) * I;
+    return a - x * b;
+}
+complex_t fact_complex(complex_t a) {
+    return tgammal(cabsl(a));
+}
+
 static operator_t OPERATORS[] = {
     {.sym='+', .type=OP_BINARY, .priority=0, .fc.c2=add_complex},
     {.sym='-', .type=OP_BINARY, .priority=0, .fc.c2=sub_complex},
     {.sym='*', .type=OP_BINARY, .priority=1, .fc.c2=mul_complex},
     {.sym='/', .type=OP_BINARY, .priority=1, .fc.c2=div_complex},
-    {.sym='%', .type=OP_BINARY, .priority=1, .fc.c2=NULL},
+    {.sym='%', .type=OP_BINARY, .priority=1, .fc.c2=mod_complex},
     {.sym='^', .type=OP_BINARY, .priority=3, .fc.c2=cpowl},
-    {.sym='!', .type=OP_RUNARY, .priority=5, .fc.c1=NULL},
+    {.sym='!', .type=OP_RUNARY, .priority=5, .fc.c1=fact_complex},
     {.sym='+', .type=OP_LUNARY, .priority=2, .fc.c1=nop_complex},
     {.sym='-', .type=OP_LUNARY, .priority=2, .fc.c1=neg_complex},
     {.sym='#', .type=OP_LUNARY, .priority=4, .fc.c1=csqrtl},
